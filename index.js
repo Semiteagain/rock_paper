89dc5153fa29a6f093532282e76a4e345b7e9c3e
Semiu Mukaila <48639762+Semiteagain@users.noreply.github.com>
2025-05-03 12:16:20 +0100

    get value of compter choices using the math.radom with the help of chatGPT to explained the concept better

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("hello world");
+function getComputerChoice() {
+  const comChoice = ["rock", "scissor", "paper"];
+  const comIndex = Math.floor(Math.random() * comChoice.length);
+  return comChoice[comIndex];
+}
+
+console.log(getComputerChoice());
