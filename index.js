a662dd304da3553177f110a81c719ef9a4f63feb
Semiu Mukaila <48639762+Semiteagain@users.noreply.github.com>
2025-05-06 10:51:28 +0100

    using math.radom and window prompt for rock, paper and scissors projects

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 function getComputerChoice() {
-  const comChoice = ["rock", "scissor", "paper"];
-  const comIndex = Math.floor(Math.random() * comChoice.length);
-  return comChoice[comIndex];
+  const computerItems = ["rock", "paper", "scissors"];
+  const computerIndex = Math.floor(Math.random() * computerItems.length);
+  return computerItems[computerIndex];
 }
 
 // console.log(getComputerChoice());
 
 function getHumanChoice() {
-  const getChoice = Number(window.prompt("click to play", ""));
-  return getChoice();
+  const getHumanPrompt = getComputerChoice(
+    window.prompt("get window prompt", "")
+  );
+  return getHumanPrompt;
 }
-console.log(getHumanChoice());
 
-// declare player score
+// console.log(getHumanChoice());
 
 const humanScore = 0;
 const computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+  for (let i =1; i <= )
+}
